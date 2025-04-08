d9222a43b59279c4f6caf4c24f0cdec576283cd2
PiyushJain045 <lightningiit123@@gmail.com>
2025-04-09 00:05:43 +0530

    module2

--- backend_core/e_commerce/views.py
+++ backend_core/e_commerce/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Product 
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        print("INSIDE GET HOME")
+
+        products = Product.objects.all() 
+
+        print(products[0].price)
+
+        context = {
+            'products': products 
+        }
+
+
+        return render(request, "home.html", context)
+
+    def post(self, request):
+        pass
